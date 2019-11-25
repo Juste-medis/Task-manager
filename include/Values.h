c0a3d45fed9b47004a7f45ff60f1935025dbdf4b
Juste-medis <52033394+Juste-medis@users.noreply.github.com>
2019-11-25 22:34:40 +0100

    Add files via upload

---
+++ include/Values.h
@@ -0,0 +1,20 @@
+#ifndef VALUES_H
+#define VALUES_H
+#include <iostream>
+#include <string>
+#include <tache.h>
+#include <list>
+
+using namespace std ;
+class Values
+{
+    private:
+    protected:
+    public:
+        Values();
+        string afficher_condition() ;
+        string afficher_accueil();
+        string cadran_depart() ;
+};
+
+#endif // VALUES_H
